c29b0ad973fcf2612186de452657f96b3572ac9a
H3NL3Y <33662710+H3NL3Y@users.noreply.github.com>
2025-02-16 23:32:36 +0000

    Update Hangman.py
    Completed basic version of the challenge.

--- Hangman.py
+++ Hangman.py
@@ -1,41 +1,49 @@
 #Hangman
 import random
 
+def win():
+    print("YOU GUESSED THE WORD!")
+def hanged():
+    print("YOU HAVE BEEN HANGED!")
 
 def play(word):
     guessedLetters = []
-    guessedWords = []
-    guessed = False
-    display = ""
+
     fullWord = "_"*len(word) #Creates placeholder for word using _
+    print(fullWord)
 
-
-    while guessed == False:
-        print(fullWord)
-        #User guesses first letter.
-        guess = input("Enter your guess: ")
-        for letter in word:
-            if letter == guess:
+    count = 0
+    while True:
+        display = ""
+        guess = input("Enter your guess: ").lower()
+        if guess == word: #Allows user to guess full word.
+            win()
+            break
+        for letter in word: #Checks each letter in word.
+            if letter in guessedLetters:
+                display += letter #If matched it will add letter to display
+            elif letter == guess:
+                guessedLetters.append(guess)
+                print(guessedLetters)
                 display += guess
-                print(display)
             else:
-                display += "_"
-                print(display)
-                guessedLetters += guess
-                if len(guessedLetters) == 11:
-                    print("You are dead. GAME OVER")
-                    guessed = True
-
-    print(display)
+                display += "_" #If no match then add _
+        count += 1
+        print(display)
+        if count == 7: #Provides 7 chances.
+            win()
+            break
+        elif display == word:
+            hanged()
+            break
 
 # TODO:: Find location of the found letter. COMPLETE
 # TODO:: Replace letters to _ and replace when found. COMPLETE
-# TODO:: Check win/loss condition.
-#11 Parts
-
+# TODO:: Check win/loss condition. COMPLETE
+# TODO:: Allow user to guess full word. COMPLETE
 
 #Choose random word.
-words = ["Cheese","Bread"]
+words = ["cheese","bread"]
 word = random.choice(words)
 word.lower()
 print(word)
