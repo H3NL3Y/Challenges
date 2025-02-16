ba7852dc48346625ef2a13e6654c01ab638f22c7
H3NL3Y <33662710+H3NL3Y@users.noreply.github.com>
2025-02-16 20:29:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-H3NL3Y/Hangman
+#Hangman
+import random
+
+
+def play(word):
+    guessedLetters = []
+    guessedWords = []
+    guessed = False
+    display = ""
+    fullWord = "_"*len(word) #Creates placeholder for word using _
+
+    print(fullWord)
+    #User guesses first letter.
+    guess = input("Enter your guess: ")
+    for letter in word:
+        if letter == guess:
+            display += guess
+        else:
+            display += "_"
+            guessedLetters += guess
+            if len(guessedLetters) == 11:
+                print("You are dead. GAME OVER")
+
+    print(display)
+
+# TODO:: Find location of the found letter. COMPLETE
+# TODO:: Replace letters to _ and replace when found. COMPLETE
+# TODO:: Check win/loss condition.
+#11 Parts
+
+
+#Choose random word.
+words = ["Cheese","Bread"]
+word = random.choice(words)
+word.lower()
+print(word)
+play(word)
